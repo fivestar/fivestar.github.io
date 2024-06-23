459f2fc4677298a851df4eeb357ce1234d5acdfb
Katsuhiro Ogawa <ko.fivestar@gmail.com>
2024-06-23 13:50:58 +0900

    Update deploy workflow

--- app/src/Kana/KanaApp.tsx
+++ app/src/Kana/KanaApp.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import "./Kana.css";
 import { SourceField } from './SourceField';
 import { DestField } from './DestField';
--- app/src/Timer/TimerApp.tsx
+++ app/src/Timer/TimerApp.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import { useState, useEffect } from 'react';
 import { useInterval } from 'usehooks-ts'
 import "./Timer.css";
 import { TimeField } from './TimeField';
