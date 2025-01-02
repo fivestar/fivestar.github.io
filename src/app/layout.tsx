8ebc09731d07287dbe647ef0530c1d6abc2e7aad
Katsuhiro Ogawa <ko.fivestar@gmail.com>
2025-01-02 15:45:41 +0900

    fix: resolve FA flashing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,11 @@ import './globals.css';
 import { titleAndDescription } from './shared-metadata';
 import { SiteFooter } from './SiteFooter';
 
+// https://docs.fontawesome.com/web/use-with/react/use-with
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+config.autoAddCss = false;
+
 export const metadata: Metadata = {
   ...titleAndDescription({
     title: 'fvstr.jp',
